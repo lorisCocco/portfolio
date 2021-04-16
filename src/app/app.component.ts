7775e2cd5621067517709b97c6cb7304636c3507
Loris Cocco <loris.cocco98@gmail.com>
2021-04-16 14:56:09 +0200

    Dark/Light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+type Theme = 'light' | 'dark';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentTheme: Theme = 'light';
   title = 'app';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.document.body.classList.add(this.currentTheme);
+  }
+  switchTheme(newTheme: Theme): void {
+    this.document.body.classList.replace(this.currentTheme, newTheme);
+    this.currentTheme = newTheme;
+  }
 }
